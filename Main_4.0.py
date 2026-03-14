94e477aa0ab8809b9a897a9d62189ccb9f0cb294
Adarsh S <adarshs112004@gmail.com>
2026-03-14 21:59:36 +0530

    chore: Add `allow_unsafe_werkzeug=True` parameter to `socketio.run`.

--- Main_4.0.py
+++ Main_4.0.py
@@ -515,4 +515,4 @@ if __name__ == '__main__':
     log.info("Starting E-Waste Sorter application.")
     threading.Thread(target=camera_thread, daemon=True).start()
     threading.Thread(target=inference_worker, daemon=True).start()
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, use_reloader=False,allow_unsafe_werkzeug=True)
